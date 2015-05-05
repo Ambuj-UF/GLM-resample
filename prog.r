65ab112150897df7cdd1bf09480866dff92eae2d
Ambuj-UF <sendambuj@gmail.com>
2015-05-04 20:18:49 -0400

    update

--- prog.r
+++ prog.r
@@ -1,27 +1,3 @@
-################################################################################################################
-# This program was written for creating running GLM analysis on the unique patterns obtained from the  data    #
-# sample of relatedness between parasitic offspring and host mothers obtained by Ye Gong,  Kimball-Brain lab   #
-# group, Biology Department, University of Florida}                                                            #
-#                                                                                                              #
-# Copyright (C) {2014}  {Ambuj Kumar, Kimball-Brain lab group, Biology Department, University of Florida}      #
-#                                                                                                              #
-# This program is free software: you can redistribute it and/or modify                                         #
-# it under the terms of the GNU General Public License as published by                                         #
-# the Free Software Foundation, either version 3 of the License, or                                            #
-# (at your option) any later version.                                                                          #
-#                                                                                                              #
-# This program is distributed in the hope that it will be useful,                                              #
-# but WITHOUT ANY WARRANTY; without even the implied warranty of                                               #
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                                                #
-# GNU General Public License for more details.                                                                 #
-#                                                                                                              #
-# This program comes with ABSOLUTELY NO WARRANTY;                                                              #
-# This is free software, and you are welcome to redistribute it                                                #
-# under certain conditions;                                                                                    #
-#                                                                                                              #
-################################################################################################################
-
-
 require(ggplot2)
 require(sandwich)
 require(msm)
@@ -53,5 +29,9 @@ x <- sort(pvalue)
 
 y=dnorm(x,mean=mean(x),sd=sd(x))
 
-plot(x,y,type="l",lwd=2,col="red")
+plot(x,y,type="l",lwd=2,col="black", xlab = "P value", ylab = "Dnorm of p values")
 
+
+x <- sort(rVal)
+y=dnorm(x,mean=mean(x),sd=sd(x))
+plot(x,y,type="l",lwd=2,col="black", xlab = "Relatedness", ylab = "Dnorm of relatedness")
